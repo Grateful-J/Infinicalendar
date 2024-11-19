611c10bebb129c1a865053fb978a749c26100c4c
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-11-18 22:11:58 -0500

    init card component

---
+++ src/components/CalendarCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <Card style="width: 25rem; overflow: hidden">
+    <template #header>
+      <img alt="user header" src="./assets/images/holidayVeg.webp" />
+    </template>
+    <template #title>{{ cardTitle }}</template>
+    <template #subtitle>{{Card subtitle}}</template>
+    <template #content>
+      <p class="m-0">
+        {{ cardContent }}
+      </p>
+    </template>
+    <template #footer>
+      <div class="flex gap-4 mt-1">
+        <Button label="Cancel" severity="secondary" outlined class="w-full" />
+        <Button label="Save" class="w-full" />
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import Card from "primevue/card";
+import Button from "primevue/button";
+
+const cardTitle = ref("Card Title");
+const cardSubtitle = ref("Card Subtitle");
+const cardContent = ref("Card Content");
+</script>
